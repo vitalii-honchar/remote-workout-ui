d546549f02d82efa70f907b5aaaa2df0ac8c3a9b
Vitalii Honchar <weaxme@gmail.com>
2022-07-31 19:17:50 +0200

    Moved markup to vue components

---
+++ src/components/StudentsPage.vue
@@ -0,0 +1,18 @@
+<script>
+import PageTemplate from './PageTemplate'
+import ListItems from "@/components/ListItems";
+
+export default {
+  name: "StudentsPage",
+  components: {
+    PageTemplate,
+    ListItems
+  }
+}
+</script>
+
+<template>
+  <page-template title="Students">
+    <list-items></list-items>
+  </page-template>
+</template>
