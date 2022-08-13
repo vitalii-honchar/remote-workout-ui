6c1e021fb73878d7784f940fefb47c1cf9faa06a
Vitalii Honchar <weaxme@gmail.com>
2022-08-13 12:02:25 +0200

    Added env variable for backend address

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:3000',
+        target: process.env.BACKEND_URL || 'http://localhost:3000',
         pathRewrite: {
           '^/api': ''
         },
